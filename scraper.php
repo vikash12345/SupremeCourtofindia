a4191715457669481b1f9a83c6af44c7905b618c
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-02-09 13:17:10 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -2,26 +2,24 @@
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 
-$years	= array('2000');
+$years	= array('1950');
 
 for ($mainpage = 0; $mainpage < sizeof($years); $mainpage++)
 {
-	$x = 1;
-	$link	=	'http://supremecourtofindia.nic.in/php/case_status/case_status_process.php?d_no='.$x.'&d_yr='.$years[$mainpage];
-	$html	=	file_get_html($link);
-	$check	=	$html->find("h5[plaintext^=Diary No]",0)->plaintext;
+	$linkabc	=	'http://supremecourtofindia.nic.in/php/case_status/case_status_process.php?d_no='.'1'.'&d_yr='.$years[$mainpage];
+	$html		=	file_get_html($linkabc);
+	$check		=	$html->find("h5[plaintext^=Diary No]",0)->plaintext;
+	$x = 1; 
 	while($check != null || $check != "")
 	{
-		foreach($html->find("/html/body")as $element)
-		{
+		$link	=	'http://supremecourtofindia.nic.in/php/case_status/case_status_process.php?d_no='.$x.'&d_yr='.$years[$mainpage];
+		$html	=	file_get_html($link);
 		$check	=	$html->find("h5[plaintext^=Diary No]",0)->plaintext;
 		$record = array( 'check' =>$check, 'link' => $link);
 		scraperwiki::save(array('check','link'), $record);
 		echo "$check\n";
 		$x++;
-		}
-	
+		
 	}
-	
 }
 ?>

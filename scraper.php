e2c6d7d343778997ae429dcf3026c182cc4506e3
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-02-09 10:43:07 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -2,26 +2,22 @@
 // This is a template for a PHP scraper on morph.io (https://morph.io)
 // including some code snippets below that you should find helpful
 
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
-//
-// // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
-//
-// // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
-//
-// // Write out to the sqlite database using scraperwiki library
-// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
-//
-// // An arbitrary query against the database
-// scraperwiki::select("* from data where 'name'='peter'")
+require 'scraperwiki.php';
+require 'scraperwiki/simple_html_dom.php';
 
-// You don't have to do things with the ScraperWiki library.
-// You can use whatever libraries you want: https://morph.io/documentation/php
-// All that matters is that your final data is written to an SQLite database
-// called "data.sqlite" in the current working directory which has at least a table
-// called "data".
+$years	= array('1950');
+
+for ($mainpage = 0; $mainpage < sizeof($years); $mainpage++)
+{
+	$x = 1;
+	do {
+	$link	=	'http://supremecourtofindia.nic.in/php/case_status/case_status_process.php?d_no='.$x.'&d_yr='.$years[$mainpage];
+	echo $html	=	file_get_html($link);
+	//$check	=	$html->find("h5[plaintext^=Diary No]",0);
+	
+	echo $check;
+    $x++;
+	} 
+	while ($x <= 5 /*$check == "" || check == null*/);
+}
 ?>

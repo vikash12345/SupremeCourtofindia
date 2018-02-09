cb924e8757f8cf44e5c527fe006b2241986f01f0
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-02-09 12:08:59 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -10,14 +10,15 @@ for ($mainpage = 0; $mainpage < sizeof($years); $mainpage++)
 	$link	=	'http://supremecourtofindia.nic.in/php/case_status/case_status_process.php?d_no='.$x.'&d_yr='.$years[$mainpage];
 	$html	=	file_get_html($link);
 	$not	=	$html->find("font[plaintext^=Case Not Found]",0)->plaintext;
-	while ($not != "Case Not Found")
+	echo "$not\n";
+	/*while ($not != "Case Not Found")
 	{	
 		$check	=	$html->find("h5[plaintext^=Diary No]",0)->plaintext;
 		$record = array( 'check' =>$check, 'link' => $link);
 		scraperwiki::save(array('check','link'), $record);
 		echo "$check\n";
 		$x++;
-	}
+	}*/
 	
 	
 }

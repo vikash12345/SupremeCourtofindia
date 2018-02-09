0067e7aeca470532a510253d58a679fb7115e48a
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-02-09 14:57:04 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -15,8 +15,8 @@ for ($mainpage = 0; $mainpage < sizeof($years); $mainpage++)
 		$link	=	'http://supremecourtofindia.nic.in/php/case_status/case_status_process.php?d_no='.$x.'&d_yr='.$years[$mainpage];
 		$html	=	file_get_html($link);
 		$check	=	$html->find("h5[plaintext^=Diary No]",0)->plaintext;
-		$record = array( 'check' =>$check, 'link' => $link);
-		scraperwiki::save(array('check','link'), $record);
+		$record = array('link' => $link 'check' =>$check);
+		scraperwiki::save(array('link','check'), $record);
 		echo "$check\n";
 		$x++;
 		

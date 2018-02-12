7570fff562add7b30ae95b79a32fbc22d8bb8b1c
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-02-12 10:43:33 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -10,14 +10,14 @@ for ($mainpage = 0; $mainpage < sizeof($years); $mainpage++)
 	$checking		=	$htmlcheck->find("h5[plaintext^=Diary No]",0)->plaintext;
 	$not			=	$htmlcheck->find("h5[plaintext^=Case Not Found]",0)->plaintext;
 	$loop			=	1;
-	$record = array('link' => $link ,  'check' =>$check);
-		scraperwiki::save(array('link','check'), $record);
+	
 	while($checking	==	'Diary No.- '.$loop. '-' .$years[$mainpage]) 
 	{
   		$link	=	'http://supremecourtofindia.nic.in/php/case_status/case_status_process.php?d_no='.$loop.'&d_yr='.$years[$mainpage];
 		$html	=	file_get_html($link);
 		$check	=	$html->find("h5[plaintext^=Diary No]",0)->plaintext;
-		
+		$record = array('link' => $link ,  'check' =>$check);
+		scraperwiki::save(array('link','check'), $record);
 		echo "$check\n";
 		$loop++;
 	} 

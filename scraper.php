dbb2359a5b3f5545386d02c6fa26c37c579e67aa
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-02-15 17:47:16 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -17,13 +17,32 @@ for ($mainpage = 0; $mainpage < sizeof($years); $mainpage++)
 	$name			=	$htmlcheck->find("h5[2]",0)->plaintext;
 	$dairyno		=	$htmlcheck->find("//*[@id='collapse1']/div/table/tbody/tr[1]/td[2]/div",0)->plaintext;
 	$cno			=	$htmlcheck->find("//*[@id='collapse1']/div/table/tbody/tr[2]/td[2]/div",0)->plaintext;
-		
+	$presentlast		=	$htmlcheck->find("//*[@id='collapse1']/div/table/tbody/tr[3]/td[2]",0)->plaintext;
+	$status			=	$htmlcheck->find("//*[@id='collapse1']/div/table/tbody/tr[4]/td[2]/font",0)->plaintext;
+	$dtype			=	$htmlcheck->find("//*[@id='collapse1']/div/table/tbody/tr[5]/td[2]/font",0)->plaintext;
+	$cat			=	$htmlcheck->find("//*[@id='collapse1']/div/table/tbody/tr[6]/td[2]",0)->plaintext;
+	$act			=	$htmlcheck->find("//*[@id='collapse1']/div/table/tbody/tr[7]/td[2]",0)->plaintext;
+	$patone			=	$htmlcheck->find("//*[@id='collapse1']/div/table/tbody/tr[8]/td[2]/p",0)->plaintext;
+	$pattwo			=	$htmlcheck->find("//*[@id='collapse1']/div/table/tbody/tr[9]/td[2]/p",0)->plaintext;
+	$adv			=	$htmlcheck->find("//*[@id='collapse1']/div/table/tbody/tr[10]/td[2]/p",0)->plaintext;
+	$radv			=	$htmlcheck->find("//*[@id='collapse1']/div/table/tbody/tr[11]/td[2]",0)->plaintext;
+	$usec			=	$htmlcheck->find("//*[@id='collapse1']/div/table/tbody/tr[12]/td[2]",0)->plaintext;
 		
 		scraperwiki::save_sqlite(array('num'), array('num' => $checking,
  'link' => trim($linkabc),
  'name' => trim($name),
  'dairyno'=> trim($dairyno),
 'cno'=> trim($cno),
+'presentlast'  => trim($presentlast),
+'status'  => trim($status),	
+'dtype'  => trim($dtype),
+'cat'  => trim($cat),
+'$act'  => trim($act),
+'patone'  => trim($patone),	
+'pattwo'  => trim($pattwo),
+'adv'  => trim($adv),
+'radv'  => trim($radv),
+'usec'  => trim($usec)					     
 ));
 	}
 		

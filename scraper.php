1b1cb99b60b4f51fc94f30f259c7821bfa51d92d
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-02-15 17:29:25 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -16,13 +16,14 @@ for ($mainpage = 0; $mainpage < sizeof($years); $mainpage++)
 	$checking		=	$htmlcheck->find("h5[plaintext^=Diary No]",0)->plaintext;
 	$name			=	$htmlcheck->find("h5[2]",0)->plaintext;
 	$dairyno		=	$htmlcheck->find("//*[@id='collapse1']/div/table/tbody/tr[1]/td[2]/div",0)->plaintext;
-
+	$cno			=	$htmlcheck->find("//*[@id='collapse1']/div/table/tbody/tr[2]/td[2]/div",0)->plaintext;
 		
 		
 		scraperwiki::save_sqlite(array('num'), array('num' => $checking,
  'link' => trim($linkabc),
  'name' => trim($name),
- 'dairyno'=> trim($dairyno)
+ 'dairyno'=> trim($dairyno),
+'cno'=> trim($cno),
 ));
 	}
 		

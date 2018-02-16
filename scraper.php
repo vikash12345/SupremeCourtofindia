f077f4d29d3c6a48cf6ad1c4fca7ebb36ed87521
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-02-16 11:06:58 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,7 +1,31 @@
 <?
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
-$years	= array('2000');
+$years	= array('1995',
+'1996',
+'1997',
+'1998',
+'1999',
+'2000',
+'2001',
+'2002',
+'2003',
+'2004',
+'2005',
+'2006',
+'2007',
+'2008',
+'2009',
+'2010',
+'2011',
+'2012',
+'2013',
+'2014',
+'2015',
+'2016',
+'2017',
+'2018'
+);
 for ($mainpage = 0; $mainpage < sizeof($years); $mainpage++)
 {
 	$RecordLoop =  0;
